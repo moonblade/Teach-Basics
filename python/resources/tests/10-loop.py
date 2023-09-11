93aafc59d90b466c2564f522e91e30c98081e98b
mnishamk <mnishamk@commvault.com>
2023-09-11 17:48:08 +0530

    loops

---
+++ python/resources/tests/10-loop.py
@@ -0,0 +1,1 @@
+# Read a number, print that number and the ten numbers after that number. Do it with a loop. use a while loop
---
+++ python/resources/tests/11-forloop.py
@@ -0,0 +1,1 @@
+# Print the odd numbers from userinput1 to userinput2, using a for loop
